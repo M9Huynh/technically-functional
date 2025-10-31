019b3ce848584f5cc7a3a5452738f73b7154ff30
Vaisnavi Shanthamoorthy <vaisshan@gmail.com>
2025-10-31 16:57:26 -0400

    add constuctor:

--- src/pose detection/anlayze.py
+++ src/pose detection/anlayze.py
@@ -4,6 +4,10 @@ import os
 os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 
 class Analyzer:
+    def __init__(self):
+        self.started = False
+        self.min_angle = 0.0
+        self.max_angle = 0.0
 
 
 
