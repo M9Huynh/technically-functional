0af9686d7bfc2a98e27a395940c0b6d1363fc660
Vaisnavi Shanthamoorthy <vaisshan@gmail.com>
2025-11-01 16:48:37 -0400

    add helper function to calculate knee angle

--- src/pose detection/main.py
+++ src/pose detection/main.py
@@ -1,11 +1,35 @@
 import cv2 as cv
 import mediapipe as mp
+import math
 import os
 from draw import PoseCamera
 from analyze import Analyzer
 
 os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "1"
 
+def _angle_deg(a, b, c):
+    if a is None or b is None or c is None:
+        return None
+    bax, bay = a[0]-b[0], a[1]-b[1]
+    bcx, bcy = c[0]-b[0], c[1]-b[1]
+    na = math.hypot(bax, bay); nc = math.hypot(bcx, bcy)
+    if na == 0 or nc == 0:
+        return None
+    cosv = max(-1.0, min(1.0, (bax*bcx + bay*bcy)/(na*nc)))
+    return math.degrees(math.acos(cosv))
+
+def knee_angle_from_result(res, side="RIGHT"):
+    if not res or not res.pose_landmarks:
+        return None
+    lm = res.pose_landmarks.landmark
+    if side.upper() == "RIGHT":
+        hip_i, knee_i, ankle_i = 24, 26, 28
+    else:
+        hip_i, knee_i, ankle_i = 23, 25, 27
+    hip   = (lm[hip_i].x,   lm[hip_i].y)
+    knee  = (lm[knee_i].x,  lm[knee_i].y)
+    ankle = (lm[ankle_i].x, lm[ankle_i].y)
+    return _angle_deg(hip, knee, ankle)
 class PoseApp:
     def __init__(self):
         self.pose_detect = mp.solutions.pose.Pose()
