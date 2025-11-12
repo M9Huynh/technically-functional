6c327077411842efc099b64f30d9ccae8962342b
Vaisnavi Shanthamoorthy <vaisshan@gmail.com>
2025-11-11 21:36:07 -0500

    fixed calibration state to display values

--- src/pose detection/analyze.py
+++ src/pose detection/analyze.py
@@ -31,7 +31,6 @@ class Analyzer:
         if self.cal_max is None or angle > self.cal_max:
             self.cal_max = angle
 
-
         if (time.time() - self.cal_start_ts) >= self.cal_duration_s:
             if self.cal_min is not None and self.cal_max is not None:
                 self.min_angle = self.cal_min
@@ -40,11 +39,17 @@ class Analyzer:
             self.is_calibrating = False
 
     def update(self, angle:float):
+
+        if self.is_calibrating:
+            self._calibrate_with_angle(angle)
+            return
+        
         #Track the first detected angle to initialize the angle variable
         if not self.started:
             self.min_angle = angle
             self.max_angle = angle
             self.started = True
+            
         #update min angle when knee bends more
         if angle < self.min_angle:
             self.min_angle = angle        
@@ -61,6 +66,11 @@ class Analyzer:
             "min_degree": round(self.min_angle, 1),
             "max_degree": round(self.max_angle, 1),
             "rom_degree": round(self.calc_rom(), 1),
+            "calibrating": self.is_calibrating,
+            "cal_time_left": (
+                max(0.0, self.cal_duration_s - (time.time() - self.cal_start_ts))
+                if self.is_calibrating else 0.0
+            ),
         }
     
 
