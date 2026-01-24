d41e7950bead696924759e86aec0a025f2403527
Vaisnavi Shanthamoorthy <vaisshan@gmail.com>
2026-01-23 19:47:38 -0500

    delete explore page and add all tabs necessary

--- mobile/app/(tabs)/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Explore() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 24, fontWeight: "700" }}>Explore</Text>
-    </View>
-  );
-}
