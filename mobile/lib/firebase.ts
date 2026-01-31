8761a7e6e713009e21babd1d330ef1917b5f1516
Vaisnavi Shanthamoorthy <vaisshan@gmail.com>
2026-01-31 00:23:10 -0500

    fix firebase to have env vars

--- mobile/lib/firebase.ts
+++ mobile/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDyAMzESiH-0w3CQCXyeJ4neie4jzGtiy8",
-  authDomain: "physiocompaion.firebaseapp.com",
-  projectId: "physiocompaion",
-  storageBucket: "physiocompaion.firebasestorage.app",
-  messagingSenderId: "750649514141",
-  appId: "1:750649514141:web:0139ac74b0f18a51f3dc74",
-  measurementId: "G-M40FYYN2Q5"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
